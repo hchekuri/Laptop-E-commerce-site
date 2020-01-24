aa16c5dd9ef365320fd0aa8eff2a23a9422d6c22
Harshini Chekuri <Harshini Chekuri>
2020-01-24 16:36:11 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixelbook",
+    img: "img/product-1.png",
+    price: 889.00,
+    company: "GOOGLE",
+    info:
+      "Meet Google Pixelbook, the high performance Chromebook. It’s the first laptop with the Google Assistant built in. The long lasting battery delivers up to 10 hours of use and it’s fast charging so can get 2 hours of use in 15 minutes. 4 in 1 design adapts to whatever you’re doing with laptop, tablet, tent and entertainment modes. Access to favorite apps including Google Drive, Docs, Sheets, Slides, Gmail, YouTube and more. Pixelbook's super thin and lightweight design measures 10.3 mm and weighs 2.45lbs. Features a 12.3” 360° touchscreen display, a sleek aluminum body, Corning Gorilla Glass and a backlit keyboard. Powered by Chrome OS with automatic software updates so you’ll always have the latest virus protection. It starts up in less than 10 secs, stays fast throughout the day, and won’t slow down over time.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung Notebook 9",
+    img: "img/product-2.png",
+    price: 1399.99,
+    company: "SAMSUNG",
+    info:
+      "You finally have the power to work the way you want. The amazingly sleek 2-in-1 Notebook 9 Pen rotates 360 degrees and comes with the game-changing S Pen, which gives you more ways to be productive. Touch, type, sketch or write—however you choose to express yourself, you’ll also get more done with the latest Intel8th Generation i7 processor, Gigabit Wi-Fi, and up to 15hours of battery life* for uninterrupted work and play.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Dell Inspiron",
+    img: "img/product-3.png",
+    price: 599.99,
+    company: "Dell",
+    info:
+      "Operating System: Windows 10 Home in S Mode, 64-bitProcessor: 8th Gen Intel Quad-Core i5-8265U up to 3. 9GHzMemory: 8GB DDR4 RAMDrives: 1TB HDD + 256GB PCIE SSDNo DVD driveDisplay & Resolution: 15. 6-inch HD touchscreen 1366x768Communications: 802. 11ac WiFi Graphics (integrated): Intel UHD Graphics 620Ports & Slots: 1 USB 2. 0 Type A Ports 2x USB 3. 1 Gen 1 Type-A 1x HDMI Color: blackBattery: 3-cell Lithium-ion battery Additional Information: Dimensions: 15 x 8. 9 x 0. 8” Weight: 4. 4lb AccessoriesEST 320GB External Hard Drive Portable.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Lenovo Ideapad S340",
+    img: "img/product-4.png",
+    price: 799.00,
+    company: "Lenovo",
+    info:
+      "The IdeaPad S340 brings your audio experience alive with the theater-like sound of Dolby Audio. You'll also get the most of your display, thanks to its slim top and side bezels and FHD resolution.Meet your productivity needs with this Lenovo IdeaPad S340 Laptop. The quad-core processor promotes smooth, seamless multitasking, and wireless options let you connect to the Web and your go-to peripherals with less cable clutter. Thanks to the slim, lightweight design, it's easy to take your work to go.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "ASUS Zenbook 13",
+    img: "img/product-5.png",
+    price: 774.54,
+    company: "ASUS",
+    info:
+      "The revolutionary zenbook 13 is a laptop that's ultralight, ultrathin and Ultra powerful. It's built to give you the ultimate mobility, Featuring a feather-light 2.5lbs all-metal chassis and up to 15-hour battery life. And despite its compact, ultraportable design, zenbook 13 doesn't compromise on performance: Up to 8th generation Intel Core i7 processor, 8GB of RAM and superfast 512GB PCIe SSD let you blast through all your tasks with ease, and its Harman Kardon audio system delivers powerful immersive sound. Rediscover true freedom with the elegant and sophisticated zenbook 13! *The actual transfer speed of USB 3.0, 3.1 (Gen 1 and 2), and/or Type-C will vary depending on many factors including the processing speed of the host device, file attributes and other factors related to system configuration and your operating environment.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "HP Pavilion 15",
+    img: "img/product-6.png",
+    price: 645.00,
+    company: "HP",
+    info:
+      "15.6-Inch IPS Touchscreen FHD (1920x1080) 8th Generation Intel Quad Core i5-8250U 8 GB DDR4-2400 SDRAM 1 TB 5400 rpm SATA HDD Windows 10 Home 64-bit.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Microsoft Pro 7",
+    img: "img/product-7.png",
+    price: 1699.00,
+    company: "Microsoft",
+    info:
+      "At the office, at home or en route, new Surface Pro 7 adapts to the way you work with laptop-to-tablet versatility. And now, it delivers more power than ever, with a laptop-class Intel Core processor, all-day battery, Instant On, improved graphics, and standby time — plus more multitasking connections, including both USB-C and USB-A ports.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "MacBook pro",
+    img: "img/product-8.png",
+    price: 1399.99,
+    company: "apple",
+    info:
+      "The MacBook is carved out of solid aluminum, thus giving it a distinctive look and a grayish-white hue. The MacBook Air in particular has a thin and light design. There are two versions of the MacBook Air: the 11-inch, with an 11.6-inch screen; and the 13-inch, with a 13.3-inch screen. Apple splits the MacBook Pro into a 13-inch and a 15-inch, with the latter consisting of a 15.4-inch screen. Each computer has a full-size, backlit 78-key keyboard.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixelbook",
+    img: "img/product-1.png",
+    price: 889.00,
+    company: "GOOGLE",
+    info:
+      "Meet Google Pixelbook, the high performance Chromebook. It’s the first laptop with the Google Assistant built in. The long lasting battery delivers up to 10 hours of use and it’s fast charging so can get 2 hours of use in 15 minutes. 4 in 1 design adapts to whatever you’re doing with laptop, tablet, tent and entertainment modes. Access to favorite apps including Google Drive, Docs, Sheets, Slides, Gmail, YouTube and more. Pixelbook's super thin and lightweight design measures 10.3 mm and weighs 2.45lbs. Features a 12.3” 360° touchscreen display, a sleek aluminum body, Corning Gorilla Glass and a backlit keyboard. Powered by Chrome OS with automatic software updates so you’ll always have the latest virus protection. It starts up in less than 10 secs, stays fast throughout the day, and won’t slow down over time.",
+    inCart: false,
+    count: 0,
+    total: 0
+};
